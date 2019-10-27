f700776a749177415cb4d1d7f716f667a53dff21
lubremets <lubremets@gmail.com>
2019-10-27 15:58:10 +0200

    solution

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const safeCall = function(f) {
+  try {
+    f();
+    return true;
+  } catch(error) {
+      return false;
+  }
+};
+
+safeCall(() => console.log('Hello!')); // true
+safeCall(() => JSON.parse('abc')); // false
+safeCall(() => false); // true
+safeCall(() => abc); // false
